929d28040c414362a70e2948c460568538221b14
katsuyuki.toki <katuyuki.toki@gmail.com>
2022-08-20 15:27:35 +0900

    php関数の埋め込み

--- src/my-bbs/resources/views/welcome.blade.php
+++ src/my-bbs/resources/views/welcome.blade.php
@@ -44,6 +44,7 @@
                     </svg>
                 </div>
                 <h1>Laravel</h1>
+                <p><?= date('Y/m/d H:i:s'); ?></p>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
