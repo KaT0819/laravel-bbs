9b49e71d61b0ab7776d36d2b8c1bc77d8970fe26
katsuyuki.toki <katuyuki.toki@gmail.com>
2022-08-21 00:28:12 +0900

    ルーティングの設定

--- src/my-bbs/routes/web.php
+++ src/my-bbs/routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 一覧表示
+Route::Get('articles', 'ArticleController@index')->name('article.list');
+// 記事登録フォーム表示
+Route::Get('articles/create', 'ArticleController@create')->name('article.create');
+// 記事登録処理
+Route::Post('articles', 'ArticleController@store')->name('article.store');
+// 記事詳細表示
+Route::Get('articles/{id}', 'ArticleController@show')->name('article.show');
+// 記事編集フォーム表示
+Route::Get('articles/{id}/edit', 'ArticleController@edit')->name('article.edit');
+// 記事更新処理
+Route::Put('articles/{id}', 'ArticleController@update')->name('article.update');
+// 記事削除処理
+Route::Delete('articles/{id}', 'ArticleController@destroy')->name('article.destroy');
