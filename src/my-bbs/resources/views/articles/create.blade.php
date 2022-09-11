c7197a9831a140053d678a52a4dddab4b8f7b834
katsuyuki.toki <katuyuki.toki@gmail.com>
2022-09-11 10:38:15 +0900

    見た目整え

--- src/my-bbs/resources/views/articles/create.blade.php
+++ src/my-bbs/resources/views/articles/create.blade.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>create</title>
-</head>
-<body>
-    <h1>新規投稿フォーム</h1>
-    {{ Form::open(['route' => 'article.store']) }}
-    <div class="form-group">
-        {{ Form::label('title', 'タイトル:') }}
-        {{ Form::text('title', null) }}
-    </div>
-    <div class="form-group">
-        {{ Form::label('content', 'コンテンツ:') }}
-        {{ Form::text('content', null) }}
-    </div>
-    <div class="form-group">
-        {{ Form::submit('投稿する', ['class' => 'btn btn-primary']) }}
-        <a href="{{ route('article.list') }}">一覧に戻る</a>
-    </div>
 
-    {{ Form::close() }}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>create</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body style="padding: 60px">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a href="{{ route('article.list') }}" class="navbar-brand">My BBS</a>
+    </nav>
+    <div class="container">
+
+        <h1>新規投稿フォーム</h1>
+        {{ Form::open(['route' => 'article.store']) }}
+        <div class="form-group">
+            {{ Form::label('title', 'タイトル:', ['class' => 'form-label']) }}
+            {{ Form::text('title', null, ['class' => 'form-control']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('content', 'コンテンツ:', ['class' => 'form-label']) }}
+            {{ Form::text('content', null, ['class' => 'form-control']) }}
+        </div>
+        <div class="form-group mt-3">
+            {{ Form::submit('投稿する', ['class' => 'btn btn-primary']) }}
+            <a href="{{ route('article.list') }}">一覧に戻る</a>
+        </div>
+
+        {{ Form::close() }}
+    </div>
 </body>
+
 </html>
--- src/my-bbs/resources/views/articles/index.blade.php
+++ src/my-bbs/resources/views/articles/index.blade.php
@@ -1,27 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Indexページ</h1>
-    <p>{{ $messages['msg1'] }}</p>
-    <p>{{ $messages['msg2'] }}</p>
 
-    <div>
-        <a href="{{ route('article.create') }}">新規投稿</a>
+<body style="padding: 60px">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a href="{{ route('article.list') }}" class="navbar-brand">My BBS</a>
+    </nav>
+    <div class="container">
+
+        <h1>Indexページ</h1>
+        <p>{{ $messages['msg1'] }}</p>
+        <p>{{ $messages['msg2'] }}</p>
+
+
+        <h2 class="mb-3">記事一覧</h2>
+        <div>
+            <a href="{{ route('article.create') }}" class="btn btn-outline-primary mb-3">新規投稿</a>
+        </div>
+        <table class="table table-striped table-hover">
+            @foreach ($articles as $item)
+            <tr>
+                <td>
+                    <a href="{{ route('article.show', ['id' => $item->id]) }}">
+                        {{ $item->title }}
+                    </a>
+                </td>
+                <td>{{ $item->content }}</td>
+            </tr>
+            @endforeach
+        </table>
     </div>
-
-    <h2>記事一覧</h2>
-    @foreach ($articles as $item)
-        <p>
-            <a href="{{ route('article.show', ['id' => $item->id]) }}">
-                {{ $item->title }}：{{ $item->content }}
-            </a>
-        </p>
-    @endforeach
 </body>
+
 </html>
