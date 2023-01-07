70879edd2362ca1dee2a5739183a7c5135514888
Katsuyuki Toki <katuyuki.toki@gmail.com>
2023-01-08 00:40:37 +0900

    見た目修正

--- src/my-bbs/resources/views/articles/index.blade.php
+++ src/my-bbs/resources/views/articles/index.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 <div class="container">
-
     <h1>Indexページ</h1>
+    <h2 class="mb-3">記事一覧</h2>
     <p>{{ $messages['msg1'] }}</p>
     <p>{{ $messages['msg2'] }}</p>
     @include('articles.search')
 
-
-    <h2 class="mb-3">記事一覧</h2>
+    <p />
     <div>
         <a href="{{ route('article.create') }}" class="btn btn-outline-primary mb-3">新規投稿</a>
     </div>
