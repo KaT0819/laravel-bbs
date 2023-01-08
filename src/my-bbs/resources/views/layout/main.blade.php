6589f2441f69e913ccc9531340e2a70d4fa04aa0
Katsuyuki Toki <katuyuki.toki@gmail.com>
2023-01-08 21:36:14 +0900

    パーツ化

--- src/my-bbs/resources/views/layout/main.blade.php
+++ src/my-bbs/resources/views/layout/main.blade.php
@@ -2,21 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <title>Document</title>
+  @include('layout.style-sheet')
 </head>
 
-<body style="padding: 60px">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-        <a href="{{ route('article.list') }}" class="navbar-brand">My BBS</a>
-    </nav>
+<body>
+  @include('layout.nav')
 
+  <div class="container">
     @yield('content')
+  </div>
 </body>
 
 </html>
